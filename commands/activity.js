cb4d39857ae7b72c62767245936704588d2b55e0
Ian <iannmoreau@gmail.com>
2026-04-06 19:49:09 -0400

    update: mensaje activity check

--- commands/activity.js
+++ commands/activity.js
@@ -5,7 +5,7 @@ module.exports = {
     const cantidad = parseInt(args[0]);
     if (!cantidad) return message.reply("❌ Número inválido.");
 
-    const msg = await message.channel.send("✅ Reacciona:");
+    const msg = await message.channel.send("Hola @✅・Miembro Verificado de CHC:RP hoy estaremos haciendo un activity ckeck, De que consiste este activity check? tomar la actividad del servidor para proximas aperturas u otro tipo de cosas.!:");
 
     for (let i = 0; i < cantidad; i++) {
       await msg.react("✅");
